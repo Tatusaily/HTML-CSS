6f35307b2c413fde6268c8cbb4ac8305a06a9b22
Tatu <tatu.sailynoja@gmail.com>
2023-03-21 21:08:44 +0200

    kissat lentää

---
+++ Harjoitus/jskoodi.js
@@ -0,0 +1,8 @@
+function homelink(){
+    document.getElementById("products").style.display = "none";
+    document.getElementById("home").style.display = "block";
+}
+function productlink(){
+    document.getElementById("home").style.display = "none";
+    document.getElementById("products").style.display = "block";
+}
