6d7d40870a784f48aa0805b55bdfa24b3ac87175
Tatu <tatu.sailynoja@gmail.com>
2023-03-22 13:38:45 +0200

    products liikkuu

--- Harjoitus/jskoodi.js
+++ Harjoitus/jskoodi.js
@@ -1,8 +1,15 @@
 function homelink(){
-    document.getElementById("products").style.display = "none";
     document.getElementById("home").style.display = "block";
+    document.getElementById("products").style.display = "none";
+    document.getElementById("contact").style.display = "none";
 }
 function productlink(){
     document.getElementById("home").style.display = "none";
     document.getElementById("products").style.display = "block";
+    document.getElementById("contact").style.display = "none";
+}
+function contactlink(){
+    document.getElementById("home").style.display = "none";
+    document.getElementById("products").style.display = "none";
+    document.getElementById("contact").style.display = "block";
 }
